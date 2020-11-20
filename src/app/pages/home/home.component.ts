5ebb9c2fb1a78d046c18196462fac123df4e84a9
Jose Ignacio Vaqueiro Gonzalez <ignaciovaqdevelop@hotmail.com>
2020-11-20 17:16:32 -0600

    Home nuevo diseño

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,8 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
-
+  isToken;
   ngOnInit(): void {
+    var token = localStorage.getItem('jwt');
+    var refreshtoken = localStorage.getItem('refreshToken');
+    console.log('token '+ token);
+    console.log('refreshToken '+ refreshtoken);
+
+    if(token == '' || token == null || token == undefined){
+      this.isToken = false;
+    }else{
+      this.isToken = true;
+    }
   }
 
 }
